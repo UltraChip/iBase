75f2714c879264b0221535fdc2033f46bf1794d5
Chip <chip@example.com>
2026-03-08 20:12:06 -0700

    Initial commit. Minimal Viable Product reached.

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # iBase
-A simple automated catalog for tracking large amounts of unorganized images
+## A simple command-line utility for catalogging large numbers of unorganized images.
+
+### Intro
+iBase is a simple command-line utility designed to catalog large numbers of unorganized pictures. It's meant for those of us who having been hording random photographs and other images on our computers for years and have lost track of what we actually have. The end result is a nice, easy-to-search sqlite3 database containing information about every picture you've scanned.
+
+In addition to tracking other image properties (such as detected duplicate images), iBase uses your local ollama server to add LLM-generated descriptions and search tags to the database. 
+
+### Prerequisites
+- Python 3
+- SQLite3
+- An accessible Ollama server equipped with a model that can process image input, such as Gemini 3
+- The Ollama Python module
+- The tabulate Python module
+
+### Installation
+1. Clone this repository to your local computer.
+2. Set up your config file by editing sample.conf and saving it as iBase.conf. At minimum, you'll want to set:
+- "albumRoot" to whatever the root directory for your images is
+- "llmHost" to point to your Ollama server, and
+- "llmModel" to point to a model that can process image input.
+
+### How to Use
+The first thing you'll probably want to do is run an initial scan of your images in order to generate the actual database. You can do this by running:
+
+    $ python3 ibase.py -s
+
+Be aware that if you have a lot of images the scan could take an **EXTREMELY LONG TIME**. For reference: I had about 76,000 pictures and the initial scan took about 5 days. But once the scan is complete you should have a fully populated sqlite3 database with information about all your pictures. Once the initial database is populated re-running the scan will add any new images that you've added since the last scan.
+
+For basic keyword searches of the database you can use iBase itself with the -f parameter. For example, to search for images that contain apples you could enter:
+
+    $ python3 ibase.py -f apple
+
+The search is case-insensitive.
+
+For more sophisticated queries you are free to search the database directly with sqlite3's native command line utility or any other DB utility you choose. 
+
+### Database Schema
+The database schema is pretty straightforward. There is only a single table, named "images", which has the following columns:
+
+| Column   | Data Type           | Description |
+|----------|---------------------|-------------|
+| imid     | INTEGER PRIMARY KEY | Unique ID number for each image |
+| filename | TEXT UNIQUE         | The full file name and path of the image |
+| hash     | TEXT                | An MD5 hash of the image. Used for indentifying duplicate images |
+| dupeOf   | TEXT                | If iBase detects an image is a duplicate of others, the IMIDs of those images are stored here |
+| susDOS   | TEXT                | "Suspected Day of Shot". If iBase is able to parse the date/time an image was captured from its file name, it will be recorded here as a UNIX timestamp |
+| desc     | TEXT                | An LLM-generated description of the image |
+| tags     | TEXT                | An LLM-generated list of search tags. Comma-separated | 
+
+### Configuration
+Below are the options available to edit in iBase.conf. The file itself is essentially a JSON object, but with the added benefit that inline comments are supported. 
+
+| Option    | Example Setting    | Description |
+|-----------|--------------------|-------------|
+| albumRoot | /home/bob/Pictures | The root directory where your pictures are kept. iBase will scan this directory and any nested directories within in. |
+| dbfile    | ./iBase.db         | The path and name of the generated database file. |
+| logfile   | ./iBase.log        | The path and name of the log file. |
+| loglevel  | ERROR              | The verbosity level of the logging system. Recommend either ERROR for just error messages or INFO for full verbosity. |
+| llmHost   | localhost:11434    | The host and port of your Ollama server. |
+| llmModel  | gemma3:12b         | The LLM model to use for scanning. | 
+| llmTries  | 3                  | The number of times iBase will attempt to call Ollama for a given image before erroring out and moving on. |
+
+### Command-line Parameters
+This section will probably get updated regularly as new features are added, but this is the list of what command-line options are available.
+
+| Short | Long | Description |
+|----|--------------|-------------|
+| -h | --help       | Show usage information and exit. |
+| -r | --album-root | Override the default album root directory. |
+| -s | --scan       | Recursively scan all images in the album root and add any new images to the database. |
+| -f | --find       | Basic keyword search of the database. |
---
+++ ibase.py
@@ -0,0 +1,223 @@
+#!/bin/python3
+
+## IMAGE-BASE (iBASE)
+## K. Chaconas
+##
+## Iterates over an image library and leverages LLMs to build a searchable database of descriptions
+## for each image. 
+
+
+# IMPORTS AND CONSTANTS
+import os
+import sqlite3
+import ollama
+import hashlib
+import datetime
+import time
+import argparse
+import logging
+import logging.config
+from configManager import loadConfig
+from tabulate import tabulate
+
+cfile = './iBase.conf'
+
+
+# FUNCTIONS
+def buildFileList(albumRoot):
+    # Builds a list of image files by iterating over all the contents of albumRoot, including
+    # subdirectories.
+    imageTypes = (".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG")
+    fileList = []
+
+    for root, dirs, files in os.walk(albumRoot):
+        for file in files:
+            if file.endswith(imageTypes):
+                filename = (os.path.join(root, file))
+                fileList.append(os.fsdecode(os.fsencode(filename)))  # Normalizes the filename so
+                                                                     # that the DB doesn't choke on
+                                                                     # unparseable characters.
+    return fileList
+
+def initDB(filename):
+    # Initializes the sqlite database which keeps track of images and their descriptions
+    if not os.path.exists(filename):
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        tab_images = """CREATE TABLE images (
+                        imid INTEGER PRIMARY KEY,
+                        filename TEXT UNIQUE,
+                        hash TEXT,
+                        dupeOf TEXT,
+                        susDOS TEXT,
+                        desc TEXT,
+                        tags TEXT);"""
+        cursor.execute(tab_images)
+        cursor.close()
+        db.commit()
+        db.close()
+    return
+
+def buildHash(filename):
+    # Creates an MD5 hash of the given file, the same hash that would be returned if you had run
+    # an md5sum command on it manually. 
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            hashObj = hashlib.md5()
+            for chunk in iter(lambda: f.read(4096), b""):
+                hashObj.update(chunk)
+        return hashObj.hexdigest()
+    else:
+        logging.error(f"buildHash: File Not Found: {filename}")
+        quit()
+
+def scanDB(db, aRoot):
+    # Recursively scans all the images in albumRoot in order to categorize them and store their
+    # info in the database
+    logging.info("SCANNING MODE")
+    cursor  = db.cursor()
+    i       = 1
+    dPrompt = "In one or two sentences please describe what you see in this image. Provide ONLY the description and nothing else."
+    tPrompt = "Please generate one to five comma-separated tag words that apply to this image. Provide ONLY the tags and nothing else."
+
+    logging.info("Building file list...")
+    files = buildFileList(aRoot)
+    logging.info(f"Detected {len(files): ,} files.")
+
+    for file in files:
+        if i % 1000 == 0:
+            pct = i/len(files)
+            logging.info(f"Progress: {pct:.2%}%")
+        susDos  = parseDate(file)
+
+        try:
+            if not entryExists(file, db):
+                hashVal = buildHash(file)
+                dupeOf  = findDupes(hashVal, db)
+                desc    = callAI(file, dPrompt)
+                tags    = callAI(file, tPrompt).upper()
+                cursor.execute("INSERT OR IGNORE INTO images (filename, hash, dupeOf, susDOS, desc, tags) VALUES (?,?,?,?,?,?);", 
+                            (file, hashVal, dupeOf, susDos, desc, tags))
+                logging.info(f"Added {os.path.basename(file)} ({hashVal}) to the database.")
+            else:
+                cursor.execute("UPDATE images SET susDOS=? WHERE filename=?;", (susDos, file))
+            db.commit()
+        except Exception as e:
+            logging.error(f"Unable to process {file}!")
+            logging.error(f"Error: {e}")
+        i += 1
+
+    logging.info("SCAN COMPLETE. HAVE A NICE DAY")
+    cursor.close()
+    db.close()
+    quit()
+    return
+
+def callAI(file, prompt):
+    # Generates an LLM description for the given file using the ollama server.
+    tries = 0
+    while tries < conf['llmTries']:
+        try:
+            client   = ollama.Client(host=conf['llmHost'])
+            response = client.chat(model=conf['llmModel'], messages=[{"role": "user", "content": prompt, "images": [file],}])
+            return response['message']['content']
+        except Exception as e:
+            tries += 1
+            if tries == conf['llmTries']:
+                logging.error(e)
+    logging.error(f"Tried {conf['llmTries']} times for file {os.path.basename(file)} but failed.")
+    return "LLM Not Reachable"
+
+def entryExists(filename, db):
+    # Checks to see if a given file name already exists in the database.
+    cursor = db.cursor()
+    cursor.execute("SELECT imid FROM images WHERE filename=?", (filename,))
+    imid = cursor.fetchone()
+    cursor.close()
+    if imid:
+        return True
+    else:
+        return False
+
+def findDupes(h, db):
+    # Checks to see if there are any duplicate hashes already logged in the database. If yes, then
+    # return a list of duplicate imids.
+    cursor = db.cursor()
+    cursor.execute("SELECT imid FROM images WHERE hash=?", (h,))
+    imids = cursor.fetchall()
+    cursor.close()
+    return str(imids)[1:-1]
+
+def parseDate(lfile):
+    # If the filename has a recognizable naming convention based on the date/time the photo was
+    # taken, parse the timestamp out and note it down as the "Suspected Day of Shot (DoS)".
+    file = os.path.basename(lfile)
+    try:
+        if file[:4] == "PXL_":
+            year   = int(file[4:8])
+            month  = int(file[8:10])
+            day    = int(file[10:12])
+            hour   = int(file[13:15])
+            minute = int(file[15:17])
+
+            dto = datetime.datetime(year, month, day, hour, minute, tzinfo=datetime.timezone.utc)
+            tstamp = time.mktime(dto.timetuple())
+            return str(tstamp)
+    except:
+        logging.error(f"parseDate failure on {lfile}. Leaving susDOS blank.")
+    return ""
+
+def searchDB(db, searchterm):
+    # Search the database for images that match the search term
+    cursor = db.cursor()
+    query  = "SELECT imid, filename, desc, tags FROM images WHERE tags LIKE ?;"
+    table  = [["ID #", "File Path", "Description", "Tags"]]
+
+    cursor.execute(query, ('%' + searchterm.upper() + '%',))
+    results = cursor.fetchall()
+    for row in results:
+        table.append([row[0], row[1], row[2], row[3]])
+    print(tabulate(table, headers='firstrow', tablefmt='grid', maxcolwidths=[None, 30, 50, 15]))
+    quit()
+    return
+
+
+# INITIALIZATION
+conf  = loadConfig(cfile)
+aRoot = conf['albumRoot']
+
+logging.config.dictConfig({    # This block silences log spam from imported modules so that only
+    'version': 1,              # my own log messages get recorded.
+    'disable_existing_loggers': True,
+})
+logging.basicConfig(
+    level=conf['loglevel'],
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(conf['logfile'], mode='a'),
+        logging.StreamHandler()])
+logging.info("INIT - Logger")
+
+initDB(conf['dbfile'])
+db = sqlite3.connect(conf['dbfile'])
+logging.info(f"INIT - Database")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--album-root", help="Override the default album root in the config file")
+parser.add_argument("-s", "--scan", action="store_true", help="Scan for updates to the database")
+parser.add_argument("-f", "--find", help="Image search")
+args = parser.parse_args()
+logging.info("INIT - Argument Parsing")
+
+
+# MAIN
+if args.album_root:
+    aRoot = args.album_root
+if args.scan:
+    scanDB(db, aRoot)
+if args.find:
+    searchDB(db, args.find)
+
+print("No parameter specified.")
+quit()
+
