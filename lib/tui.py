bb14357a5aee6ba7f9bbf92d0ebed72f4fbccabf
Chip <chip@example.com>
2026-04-08 19:18:31 -0700

    WIP - working on search TUI.

--- lib/tui.py
+++ lib/tui.py
@@ -9,10 +9,12 @@
 import sqlite3
 import datetime
 import os
+import sengine
 from time import sleep
 from tkinter import Image
 from simple_term_menu import TerminalMenu
 from PIL import Image
+from math import floor
 
 
 # FUNCTIONS
@@ -31,7 +33,7 @@ def mainmenu(db, conf):
             imidMenu(db, imid)
         elif choice == 1:
             searchTerm = input("Search term: ")
-            searchMenu(db, searchTerm)
+            searchMenu(db, searchTerm, conf)
         elif choice == 2:
             return
 
@@ -63,6 +65,66 @@ def imidMenu(db, imid):
         print("Bad IMID!")
         sleep(2)
         return
+
+def searchMenu(db, searchTerm, conf):
+    # TUI for searching records based on a search term.
+    records   = []
+    cursor    = db.cursor()
+    results   = sengine.search(searchTerm, db)[:conf['results']]
+
+    if results:
+        for result in results:
+            fullRecord = cursor.execute("SELECT imid, filename, desc FROM images WHERE imid = ?",
+                                        (result,)).fetchone()
+            records.append(fullRecord)
+        menuLines = buildRecordLines(fullRecord)
+        menuLines.append("Go back")
+
+        while True:
+            os.system('clear')
+            menu = TerminalMenu(menuLines)
+            choice = menu.show()
+            if choice == len(menuLines):
+                cursor.close()
+                return
+            else:
+                imidMenu(db, results[choice])
+    else:
+        print("No results found!")
+        sleep(2)
+        return
+
+def buildRecordLines(records):
+    # Creates nicely-formatted menu lines from a list of records.
+    recordLines = []
+    colOne      = 0
+    colTwo      = 0
+    colThree    = 0
+    try:
+        width = (os.get_terminal_size().columns) - 2
+    except:
+        width = 98
+
+    for record in records:                  # This initial loop just calculates what the column
+        if len(str(record[0])) > colOne:    # widths should be. 
+            colOne = len(str(record[0]))
+        if len(record[1]) > colTwo:
+            if len(record[1]) > width / 2:
+                colTwo = floor(width / 2)
+            else:
+                colTwo = len(record[1])
+        colThree = width - (colOne + colTwo + 3)
+    
+    for record in records:
+        imid = f" {record[0]}" + " "*(colOne-(len(str(record[0]))+1))
+        if len(record[1]) >= colTwo:
+            filename = f" {record[1][:(colTwo-1)]}"
+        else:
+            filename = f" {record[1]}" + " "*(colTwo-(len(record[1]))+1)
+
+    
+
+    return recordLines
 
 def buildHeader(content, delim="="):
     # Builds a header string.
